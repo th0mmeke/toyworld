6bdeff023703b80cdd467979c5d38d6c96c1f5cb
Thomas <tom@hensonyoung.net>
2016-02-03 16:44:55 +1300

    Added default for TOYWORLDDATA so a little more graceful if it's missing

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ Created on 9/02/2013
 
 import os
 
-DataDir = os.environ['TOYWORLDDATA']
-SrcDir = os.environ['TOYWORLD']
+DataDir = os.environ.get('TOYWORLDDATA') or os.getcwd()
 TestDir = os.path.join(DataDir, 'test')
 EnergyTolerance = 2E-2
